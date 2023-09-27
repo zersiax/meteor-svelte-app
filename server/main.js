a292dd4bc068548084dacf7297aee062508b8f0e
Florian Beijers <florianbeijers@gmail.com>
2023-09-28 00:27:30 +0200

    remove startup function

--- server/main.js
+++ server/main.js
@@ -4,25 +4,6 @@ import '/imports/api/categoriesMethods';
 import '/imports/api/categoriesPublications';
 
 const insertCategory = categoryText => {
-  CategoriesCollection.insert({ text: categoryText });
+  CategoriesCollection.insert({ text: categoryText, groceries: [] });
 }
 
-
-Meteor.startup(async () => {
-  CategoriesCollection.remove({})
-  if (await CategoriesCollection.find().countAsync() === 0) {
-    [
-      'First dinner',
-      'Second Dinner',
-      'third dinner',
-      'Fourth dinner',
-      'Fifth Dinner',
-      'Sixth Dinner',
-      'breakfast'
-
-    ].forEach(insertCategory)
-    categoriesCollection.updateMany({}, { $set: { groceries: []}});                          
-  }
-
-
-});
